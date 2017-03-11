7481bbd691427300c4b138c82d45adab9d1a3e1c
hiepnhse61627 <hiepnhse61627@gmail.com>
2017-03-11 10:53:02 +0700

    Update CartDTO And CategoryDTO

--- 04.Source/ComputerShopProject/src/main/java/com/dto/CartDTO.java
+++ 04.Source/ComputerShopProject/src/main/java/com/dto/CartDTO.java
@@ -1,7 +1,104 @@
 package com.dto;
 
+import java.util.Date;
+
 /**
  * Created by hiepnhse61627 on 08/03/2017.
  */
 public class CartDTO {
+
+    private Integer id;
+
+    private String cd;
+
+    private String customerCd;
+
+    private Float totalPrice;
+
+    private Date createdOn;
+
+    private String createdBy;
+
+    private Date modifiedOn;
+
+    private String modifiedBy;
+
+    public CartDTO() {
+
+    }
+
+    public CartDTO(Integer id, String cd, String customerCd, Float totalPrice, Date createdOn, String createdBy, Date modifiedOn, String modifiedBy) {
+        this.id = id;
+        this.cd = cd;
+        this.customerCd = customerCd;
+        this.totalPrice = totalPrice;
+        this.createdOn = createdOn;
+        this.createdBy = createdBy;
+        this.modifiedOn = modifiedOn;
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCd() {
+        return cd;
+    }
+
+    public void setCd(String cd) {
+        this.cd = cd;
+    }
+
+    public String getCustomerCd() {
+        return customerCd;
+    }
+
+    public void setCustomerCd(String customerCd) {
+        this.customerCd = customerCd;
+    }
+
+    public Float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(Date modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 }
--- 04.Source/ComputerShopProject/src/main/java/com/dto/CategoryDTO.java
+++ 04.Source/ComputerShopProject/src/main/java/com/dto/CategoryDTO.java
@@ -1,7 +1,103 @@
 package com.dto;
 
+import java.util.Date;
+
 /**
  * Created by hiepnhse61627 on 08/03/2017.
  */
 public class CategoryDTO {
+
+    private Integer id;
+
+    private String cd;
+
+    private String name;
+
+    private String description;
+
+    private Date createdOn;
+
+    private String createdBy;
+
+    private Date modifiedOn;
+
+    private String modifiedBy;
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(Integer id, String cd, String name, String description, Date createdOn, String createdBy, Date modifiedOn, String modifiedBy) {
+        this.id = id;
+        this.cd = cd;
+        this.name = name;
+        this.description = description;
+        this.createdOn = createdOn;
+        this.createdBy = createdBy;
+        this.modifiedOn = modifiedOn;
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCd() {
+        return cd;
+    }
+
+    public void setCd(String cd) {
+        this.cd = cd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(Date modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 }
